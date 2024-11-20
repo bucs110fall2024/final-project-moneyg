4ef8396bdfc8c0caa6077ca02b113d19f7a1e191
Monica Gnajewski <mgnajewski@binghamton.edu>
2024-11-20 17:08:46 -0500

    updated 11/20/24

---
+++ src/model_customer.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, x, y, img_file):
+        self.x = x
+        self.y = y
+        self.img_file = image
+        """
+        initializes the customer character (NPC)
+        args: (int) x and y coordinates of position; (str) path to jpg file of character
+        return: None
+        """
+        
+    def walk(self):
+        """
+        moves character toward counter (left)
+        args: None
+        return: None
+        """
+        
+    def order(self):
+        """
+        creates the "order" of the customer
+        args: (str) path to jpg file of order icon
+        return: None
+        """
