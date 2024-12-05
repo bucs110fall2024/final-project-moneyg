ef8eb249dc6e5e5ad216eb8b2a996b944cf733ae
Monica Gnajewski <mgnajewski@binghamton.edu>
2024-12-05 01:54:52 -0500

    99.1% done

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import pygame
 from src.controller import Controller
 
 def main():
-    #pygame.init()
+    pygame.init()
     game = Controller()
     game.mainloop()
     
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
-
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
